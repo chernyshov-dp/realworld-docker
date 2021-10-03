fc28d2656e5f223c20b8c9803a3dcd49accd1091
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-10-03 23:22:35 +0300

    Последние улучшения

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. Change from Docker.
-            We are in dev!
+            Hello from web sockets!
         </p>
         <a
           className="App-link"
